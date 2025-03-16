0c7b0b65481ca02fbf141b6c4d3faa24b450eb15
Benedikt Hoffmann <benedikt.hoffmann@gmx.at>
2025-03-16 21:10:53 +0100

    update

--- session1/conditionals.cpp
+++ session1/conditionals.cpp
@@ -1,4 +1,6 @@
+#include "../mytools.hpp"
 #include <stdio.h>
+
 
 int greateri(int a, int b)
 {
@@ -19,6 +21,51 @@ int main(){
     printf("a is greater\n") :
     printf("b is greater\n") ;
 
+    switch (a)
+    {
+        case 1: printf("1\n");
+            // other things
+            a = 3;
+            break; // <-- break required to stop switch
+        default: printf("%d\n", a); break;
+    }
+
+
+    int drink;
+    float price = 0.;
+
+    while (price == 0.)
+    {
+        printf("Getränk wählen: (1, 2, 3)\n");
+        scanf("%d", &drink);
+
+        switch (drink) {
+            case 1: price = 0.5; break;
+            case 2: price = 1.0; break;  
+            case 3: price = 2.0; break;
+            default: printf("bitte nur szwischen 1-3 wählen\n");
+        }
+    }
+
+    float paid = 0.;
+
+    while (true)
+    {
+        printf("bitte werfen sie %.2f ein (oder -1 um Abszubrechen): ", price);
+        scanf("%f", &paid);
+        if (paid == price)
+        {
+            price = 0.;
+            paid = 0;
+            printf("vielen Dank! \n");
+            break;
+        }
+        else if (paid == -1)
+        {
+            printf("Abgebrochen\n");
+            break;
+        }
+    }
 
     return 0;
 }

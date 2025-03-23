4bef1d49923a530450c535a40c87df72020e5947
Benedikt Hoffmann <benedikt.hoffmann@gmx.at>
2025-03-23 20:09:11 +0100

    test

--- mytools.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef MY_TOOLS_IMPLEMENTATION
-
-#define MY_ERROR(message) \
-        {printf("Error: %s\n in file %s at line %d\n", message, __FILE__, __LINE__);\
-         exit(-1);}
-
-#endif // MY_TOOLS_IMPLEMENTATION

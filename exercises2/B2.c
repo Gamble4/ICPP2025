b77c7eb4247ef7641c16703a64253436bb547d09
Benedikt <benedikt.hoffmann@gmx.at>
2025-04-03 14:30:05 +0200

    mini

--- exercises2/B2.c
+++ exercises2/B2.c
@@ -20,17 +20,6 @@ int ind_in_array(int* a, int ind, int n)
     return 0;
 }
 
-int ind_max(int *arr, int n)
-{
-    int i = 0;
-    for (int j=0; j<n; j++)
-    {
-        if (arr[j] > arr[i]) i = j;
-    }
-
-    return i;
-}
-
 int lucas(int n)
 {
     static int l0 = 2;
@@ -46,11 +35,17 @@ int lucas(int n)
     return l1;
 }
 
+int compare(void const *a, void const *b)
+{
+    // compare function in qsort wants void const 
+    return *(int*)a - *(int*)b;
+}
+
 
 int main()
 {
     const int max_num_terms = 10;
-    int num_terms, scanf_success, terms[max_num_terms], natural[num_terms];
+    int num_terms, scanf_success, terms[max_num_terms], natural[max_num_terms];
     int scan_tmp;
     printf("How many terms to calculate? \n");
     scanf_success = scanf("%d", &num_terms);
@@ -81,11 +76,21 @@ int main()
     }
     printf("\n");
 
-    printf("%d ", lucas(10));
     // take differences in order to compute the terms
+    qsort(natural, num_terms, sizeof(int), compare);
+    print_arr(natural, num_terms);
+    int diffs[max_num_terms];
+    diffs[0] = natural[0];
+    for (int i=0; i<num_terms-1; i++)
+    {
+        diffs[i+1] = natural[i+1] - natural[i];
+    }
+    print_arr(diffs, num_terms);
     for (int i=0; i<num_terms; i++)
     {
-        qsort(natural, num_terms, sizeof(int), void*);
-        natural[i] = lucas(natural[i+1]-natural[i]);
+        
+        natural[i] = lucas(diffs[i]);
     }
+
+    print_arr(natural, num_terms);
 }
--- exercises2/scratch.c
+++ exercises2/scratch.c
@@ -1,7 +1,50 @@
-#include <cstdint>
 #include <stdio.h>
+
+
+int lucas_static(int n)
+{
+    static int l0 = 2;
+    static int l1 = 1;
+
+    //int l0 = 2;
+    //int l1 = 1;
+
+    int tmp;    
+    for (int i=0; i<n; i++)
+    {
+        tmp = l0+l1;
+        l0 = l1;
+        l1 = tmp;
+    }
+    return l1;
+}
+
+int lucas(int n)
+{
+    int l0 = 2;
+    int l1 = 1;
+
+    int tmp;    
+    for (int i=0; i<n; i++)
+    {
+        tmp = l0+l1;
+        l0 = l1;
+        l1 = tmp;
+    }
+    return l1;
+}
+
+
 
 int main()
 {
-    printf("%d", INT32_MAX);
+    int n = 3, terms[3]={1, 2, 8};
+    int diffs[] = {1, 1, 6};
+
+
+    for (int i=0; i<3; i++)
+    {
+        printf("lucas nr for n=%d: %d \n", i, lucas(terms[i]));
+        printf("lucas static n=%d: %d \n", i, lucas_static(diffs[i]));
+    }
 }

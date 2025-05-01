7f1830a7e161c7b998923978c24afb70744e4fb3
Benedikt Hoffmann <benedikt.hoffmann@gmx.at>
2025-05-01 11:43:04 +0200

    exercise 4

---
+++ exercises4/build/CMakeFiles/D1lib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/D1lib.dir/src/D1.cpp.o"
+  "CMakeFiles/D1lib.dir/src/D1.cpp.o.d"
+  "libD1lib.a"
+  "libD1lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/D1lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
